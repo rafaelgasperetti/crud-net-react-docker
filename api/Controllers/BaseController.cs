54ad1f14069cabccd67980b28d60fae8ddae28c6
Rafael Gasperetti <rafael.gasperetti@bitzar.solutions>
2021-09-09 11:32:41 -0300

    Removidas diretrizes desnecessárias.

--- api/Controllers/BaseController.cs
+++ api/Controllers/BaseController.cs
@@ -8,8 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web.Http.Cors;
 
 namespace api.Controllers
 {
